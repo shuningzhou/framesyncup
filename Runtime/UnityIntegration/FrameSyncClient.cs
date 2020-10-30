a1819bfd68533b2bbddcbf70fbfd879a50275336
Peter <peterzhou43@gmail.com>
2020-10-30 09:20:04 -0700

    publish

--- Runtime/UnityIntegration/FrameSyncClient.cs
+++ Runtime/UnityIntegration/FrameSyncClient.cs
@@ -2,18 +2,22 @@
 using SWNetwork.Core;
 using System.Collections.Generic;
 using SWNetwork.Core.DataStructure;
+using System;
 
 namespace SWNetwork.FrameSync
 {
     public class FrameSyncClient : IFrameSyncPlayerDataProvider
     {
-        public delegate void ClientReady(bool ready);
-        public static ClientReady OnClientReadyEvent;
-
         public static FrameSyncClient Instance = null;
         private SWGameServerClient _client;
 
         static string _debugName = "SWFrameSyncClient";
+        static bool _debugMode = false;
+        static int _debugPlayerCount = 0;
+
+        static string _playerUID;
+        
+        static Action<bool> _clientReadyHandler;
 
         public static void Init(string playerUID)
         {
@@ -22,6 +26,34 @@ namespace SWNetwork.FrameSync
                 return;
             }
 
+            _debugMode = false;
+            Instance = new FrameSyncClient(playerUID);
+            UnityThread.initUnityThread();
+
+            if (UnityEngine.Application.isPlaying)
+            {
+                GameObject obj = new GameObject("_SWFrameSyncClientBehaviour");
+                //obj.hideFlags = HideFlags.HideAndDontSave;
+                SWFrameSyncClientBehaviour cb = obj.AddComponent<SWFrameSyncClientBehaviour>();
+                cb.SetClient(Instance);
+            }
+        }
+
+        public static void InitDebugMode(string playerUID, int playerCount)
+        {
+            if (Instance != null)
+            {
+                return;
+            }
+
+            if(playerCount < 1 || playerCount > 8)
+            {
+                Debug.LogError($"{_debugName}: invalid player count. (Max=8)");
+                return;
+            }
+
+            _debugMode = true;
+            _debugPlayerCount = playerCount;
             Instance = new FrameSyncClient(playerUID);
             UnityThread.initUnityThread();
 
@@ -36,12 +68,17 @@ namespace SWNetwork.FrameSync
 
         FrameSyncClient(string playerUID)
         {
-            _client = new SWGameServerClient("127.0.0.1", playerUID, "roomKey");
+            _playerUID = playerUID;
+            _playerDataBiMap.Clear();
 
             //todo: this should be create in the match making phase 
-            _playerDataBiMap.Clear();
-            _playerDataBiMap.Add("1", 1);
-            _playerDataBiMap.Add("2", 2);
+            if (_debugMode)
+            {
+                for(int i = 1; i <= _debugPlayerCount; i++)
+                {
+                    _playerDataBiMap.Add($"{i}", (byte)i);
+                }
+            }
         }
 
         public IFrameSyncIO frameSyncIO
@@ -80,10 +117,45 @@ namespace SWNetwork.FrameSync
             }
         }
 
-        public static void Connect()
+        public static void Connect(Action<bool> completionHandler)
         {
             if (Instance != null)
             {
+                if (_debugMode)
+                {
+                    Debug.LogError($"{_debugName}: Please specify debug server ip and port");
+                    return;
+                }
+
+                if(Instance._client != null)
+                {
+                    Debug.LogError($"{_debugName}: You are already connected. Please call Disconnect() to stop the current connection.");
+                    return;
+                }
+
+                _clientReadyHandler = completionHandler;
+
+                //todo connect to production game servers
+                Instance._client = new SWGameServerClient("127.0.0.1", _playerUID, "roomKey");
+                Instance._client.OnGameServerConnectionReadyEvent += OnGameServerConnectionReady;
+
+                
+                Instance._client.Connect();
+            }
+        }
+
+        public static void ConnectDebugServer(string host, Action<bool> completionHandler)
+        {
+            if (Instance != null)
+            {
+                if (Instance._client != null)
+                {
+                    Debug.LogError($"{_debugName}: You are already connected. Please call Disconnect() to stop the current connection.");
+                    return;
+                }
+
+                _clientReadyHandler = completionHandler;
+                Instance._client = new SWGameServerClient(host, _playerUID, "roomKey");
                 Instance._client.OnGameServerConnectionReadyEvent += OnGameServerConnectionReady;
 
                 Instance._client.Connect();
@@ -94,8 +166,10 @@ namespace SWNetwork.FrameSync
         {
             if (Instance != null)
             {
+                _clientReadyHandler = null;
                 Instance._client.OnGameServerConnectionReadyEvent -= OnGameServerConnectionReady;
                 Instance._client.Stop();
+                Instance._client = null;
             }
         }
 
@@ -103,15 +177,19 @@ namespace SWNetwork.FrameSync
         private static void OnGameServerConnectionReady(bool ready)
         {
             UnityThread.executeInUpdate(() =>
-            {
-                OnClientReadyEvent(ready);
+            {   
+                if(_clientReadyHandler != null)
+                {
+                    _clientReadyHandler(ready);
+                    _clientReadyHandler = null;
+                }
             });
         }
 
         internal void OnUnityApplicationQuit()
         {
-            SWConsole.Info($"{_debugName} Application ending after " + Time.time + " seconds");
             Disconnect();
+            SWConsole.Info($"{_debugName} Application ending after " + Time.time + " seconds");
         }
 
         //playerDataProvider
